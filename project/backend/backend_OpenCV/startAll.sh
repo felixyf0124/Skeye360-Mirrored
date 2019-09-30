82ba293bcb82558d1ca14006408c407c7f3459f9
Li Sun <vincent.sun870530@gmail.com>
2019-09-29 20:48:08 -0400

    delete redundent file #40

--- project/backend/backend_OpenCV/startAll.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Change file different character between windows and VM
-sudo sed -i -e 's/\r$//' install_opencv.sh
-
-# Install dependencies
-sudo bash install_opencv.sh
-
-# Execute openCV
-sudo python3 video.py
