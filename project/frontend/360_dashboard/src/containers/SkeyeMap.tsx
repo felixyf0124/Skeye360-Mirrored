f1ce832a0d45d15df966391c7d8b1f85aae5e24f
saishan <saishan.ry@gmail.com>
2019-10-08 19:33:31 -0400

    map implementation, map and simple marker #33 i

---
+++ project/frontend/360_dashboard/src/containers/SkeyeMap.tsx
@@ -0,0 +1,29 @@
+
+import React, { useState } from 'react';
+import GoogleMapReact from 'google-map-react';
+import Marker from './Marker';
+
+const SkeyeMap = (props: any) => {
+    const [center, setCenter] = useState({lat: 45.5017, lng: -73.5673 });
+    const [zoom, setZoom] = useState(11);
+
+    return (
+        <div style={{ height: '100vh', width: '100%' }}>
+
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyDF3Bsq5rm-uhEMAqqyMqzgc-dXUPl9Byw' }}
+          defaultCenter={center}
+          defaultZoom={zoom}
+        >
+          <Marker
+            lat={45.5017}
+            lng={-73.5673}
+            text="Camera_id"
+            color="red"
+          />
+        </GoogleMapReact>
+      </div>
+    );
+}
+
+export default SkeyeMap;
