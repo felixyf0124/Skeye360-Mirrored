b6cf134877cff5ce8aa5e2cee585f32cb0874f54
Batoul Yehia <batoul.yehia@gmail.com>
2019-10-11 02:46:24 -0400

    #30 Added prototype for dashboard

--- project/frontend/360_dashboard/src/components/LeftPanel.tsx
+++ project/frontend/360_dashboard/src/components/LeftPanel.tsx
@@ -4,9 +4,9 @@ import { Button } from 'react-bootstrap';
 const LeftPanel = (props: any) => {
     return(
      <div className = "left-panel-container">
-         <Button>Button 1</Button>
-         <Button>Button 2</Button>
-         <Button>Button 3</Button>
+         <Button className="panel-btn">Button 1</Button>
+         <Button className="panel-btn">Button 2</Button>
+         <Button className="panel-btn">Button 3</Button>
      </div>   
     )
 }
