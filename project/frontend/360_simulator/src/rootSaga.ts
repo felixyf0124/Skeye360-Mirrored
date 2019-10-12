7fe20aa3ce053a772042a63da3857768af5781bc
felixyf0124 <felixyf0124@gmail.com>
2019-10-12 12:00:17 -0400

    #47 merge simulator into dashboard

--- project/frontend/360_simulator/src/rootSaga.ts
+++
@@ -1,7 +0,0 @@
-import { fork } from 'redux-saga/effects';
-
-import { saga as hello } from './contexts/hello';
-
-export default function* rootSage(): IterableIterator<any> {
-  yield fork(hello);
-}
