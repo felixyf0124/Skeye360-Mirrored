28ae9eb2751ef6c4a2dc88b80c3c076d1cbe0dfe
Batoul Yehia <batoul.yehia@gmail.com>
2019-10-24 18:55:37 -0400

    #62 Fixes indentation and css for the graphs.

--- project/frontend/360_dashboard/src/components/MixedChart.tsx
+++ project/frontend/360_dashboard/src/components/MixedChart.tsx
@@ -14,56 +14,58 @@ interface ChartState {
 }
 
 class MixedChart extends Component<ChartProps, ChartState>{
-    constructor(props: ChartProps){
-        super(props);
-
-        this.state = {
-            options: {
-              stroke: {
-                width: [0, 4]
-              },
-              title: {
-                text: 'Traffic',
-                align: 'center'
-
-              },
-              labels: ['0', '2', '4', '6', '8', '10', '12', '14', '16', '18', '20', '22'
-              ],
-              xaxis: {
-                type: 'Hours',
-                title: {
-                    text: 'Hours',
-                }
-              },
-              yaxis: [{
-                title: {
-                  text: 'Number of cars',
-                },
-  
-              }, {
-                opposite: true,
-                title: {
-                    text:'max cars'
-                }
-              }]
-            },
-            series: [{
-              name: 'Total Cars',
-              type: 'column',
-              data: [300, 505, 414, 631, 257, 455, 201, 300, 744, 380, 230, 200]
-            }, {
-              name: 'Max Congestion',
-              type: 'line',
-              data: [23, 42, 35, 27, 43, 22, 17, 31, 22, 22, 12, 16]
-            }],
+  constructor(props: ChartProps){
+    super(props);
+    this.state = {
+      options: {
+        stroke: {
+          width: [0, 4]
+        },
+        title: {
+          text: 'Traffic',
+          align: 'center'
+        },
+        labels: ['0', '2', '4', '6', '8', '10', '12', '14', '16', '18', '20', '22'],
+        xaxis: {
+          type: 'Hours',
+          title: {
+            text: 'Hours',
           }
+        },
+        yaxis: [
+          {
+            title: {
+              text: 'Number of cars',
+            },
+          },
+          {
+            opposite: true,
+            title:{
+              text:'max cars'
+            }
+          }
+        ]
+      },
+      series: [
+        {
+          name: 'Total Cars',
+          type: 'column',
+          data: [300, 505, 414, 631, 257, 455, 201, 300, 744, 380, 230, 200]
+        },
+        {
+          name: 'Max Congestion',
+          type: 'line',
+          data: [23, 42, 35, 27, 43, 22, 17, 31, 22, 22, 12, 16]
         }
-        render(){
-            return (
-                <div className="bar-chart">
-                    <Chart options={this.state.options} series={this.state.series} type="line" width="700" height="" />
-                </div>
-            )
-        }
+      ],
     }
+  }
+  render(){
+    return (
+      <div className="bar-chart">
+        <Chart options={this.state.options} series={this.state.series} type="line" width="700" height="" />
+      </div>
+    )
+  }
+}
 export default MixedChart;
