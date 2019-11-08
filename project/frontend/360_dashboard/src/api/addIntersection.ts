a1eaa6a3baf3c55016ddc84192405454673926b8
Kevin L <Lin_Kevin_1995@hotmail.com>
2019-11-08 02:50:27 -0500

    #60 Added addIntersection API call

---
+++ project/frontend/360_dashboard/src/api/addIntersection.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/camelcase */
+export interface Response {
+  id: number;
+  intersection_name: string;
+  latitude: number;
+  cameras: [];
+  longitude: number;
+  district_id: number;
+}
+
+const APIDomain = '0.0.0.0:8000';
+
+const addIntersection = async (
+  intersection_name: string,
+  latitude: string,
+  longitude: string,
+  district_id: string,
+): Promise<Response> => {
+  const url = `//${APIDomain}/api/intersection/`;
+  const params = {
+    intersection_name,
+    latitude,
+    longitude,
+    district_id,
+  };
+  const settings = {
+    method: 'POST',
+    body: new URLSearchParams(params),
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  };
+  const response = await fetch(url, settings);
+  const data = (await response.json()) as Response;
+  return data;
+};
+
+export default addIntersection;
