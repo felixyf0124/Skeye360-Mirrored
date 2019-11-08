37341b80c7b1715c951c730e78c0e562dc322765
lokevin88 <lo_kevin@outlook.com>
2019-11-07 22:35:38 -0500

    #87 Predictions work and working on writing and reading from/to mongodb

--- project/data_analytics/360_python/polynomialRegression.py
+++ project/data_analytics/360_python/polynomialRegression.py
@@ -8,9 +8,9 @@ from sklearn.preprocessing  import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 
 # Day of the month
-date = np.array([1, 2, 3, 4, 5]).reshape((-1, 1))
+date = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]).reshape((-1, 1))
 # Average time to cross one intersection to the other at a specific time
-avgTime = np.array([2, 2.3, 2.2, 3, 2.6])
+avgTime = np.array([2, 2.3, 2.2, 2.5, 2.6, 2.5, 2.5, 2.8, 2.9, 2.4, 2.5, 2.7, 2.6, 2.8]).reshape((-1, 1))
 
 # Use Polynomial Regression to fit into the dataset
 polynomial = PolynomialFeatures(degree= 2)
@@ -28,7 +28,29 @@ matplot.ylabel('Average Time')
 matplot.show()
 
 # New result of Polynomial Regression prediction
-line.predict(polynomial.fit_transform(110.0))
+# The following predicts for 2 weeks
+predictions = line.predict(polynomial.fit_transform(date))
+
+
+# print(predictions[0][0])
+# print(len(predictions))
+
+# To connect to the database
+# db = client['mongoDatabase']
+# Collection are table in SQL
+# collection = db['predictions']
+# For loop to insert into MongoDB
+# for index in range(len(predictions)):
+#     collection.insert_one({"date":date[index], "prediction":predictions[index][0]})
+
+# START of retrieving data from MongoDB
+# print('----------------------------')
+# resultsRaw = collection.find({})
+# for row in resultsRaw:
+#     resultDate = str(row['date'])
+#     resultAvgTime = str(row['avgTime'])
+#     print(resultDate + " : " + resultAvgTime)
+# END of retrieving data from MongoDB
 
 
 
