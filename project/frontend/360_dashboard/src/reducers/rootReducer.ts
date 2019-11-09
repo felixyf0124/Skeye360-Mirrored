14d2308b49aa8fdedeb2571c6112536669f482ee
Batoul Yehia <batoul.yehia@gmail.com>
2019-11-09 16:02:48 -0500

    #98 Added log states to root reducers

--- project/frontend/360_dashboard/src/reducers/rootReducer.ts
+++ project/frontend/360_dashboard/src/reducers/rootReducer.ts
@@ -2,13 +2,17 @@ import { combineReducers } from 'redux';
 import { connectRouter, RouterState } from 'connected-react-router';
 import { History } from 'history';
 import authentication, { STATE as authState } from '../contexts/authentication';
+import logClick, {STATE as logState } from '../contexts/LogClicks';
+
 
 export interface RootState {
   authentication: authState;
   router: RouterState;
+  logClick: logState;
 }
 
 export default (history: History): any => combineReducers({
   authentication,
+  logClick,
   router: connectRouter(history),
 });
