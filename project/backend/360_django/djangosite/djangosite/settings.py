6b6efbc2d9ab9a86a1d996f21cdae18f78d30d6e
SaiShan <saishan.ry@gmail.com>
2019-11-10 02:42:42 -0500

    readd knox

--- project/backend/360_django/djangosite/djangosite/settings.py
+++ project/backend/360_django/djangosite/djangosite/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'djangosite',
     'django_filters',
     'django_extensions',
-    'corsheaders'
+    'corsheaders',
+    'knox',
 ]
 
 MIDDLEWARE = [
@@ -131,6 +132,7 @@ STATIC_URL = '/static/'
 # For rest framework
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
+    'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',),
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
--- project/backend/360_django/djangosite/djangosite_api/views.py
+++ project/backend/360_django/djangosite/djangosite_api/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets, permissions, filters, generics
 from django.http import HttpResponse
+from knox.models import AuthToken
 from .models import *
 from .serializers import *
 from django.contrib.auth.models import User
