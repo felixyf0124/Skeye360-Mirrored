ad8936cdf015990c89a7eb5b87b65cd05fa123c7
felixyf0124 <felixyf0124@gmail.com>
2019-11-10 23:40:45 -0500

    #64 added test for ts geometry

---
+++ project/frontend/360_dashboard/src/test/simulator/simulator_management/RoadIntersection.test.spec.js
@@ -0,0 +1,58 @@
+import RoadIntersection from '../../../containers/simulator/simulator_management/RoadIntersection';
+import Vec2 from '../../../containers/simulator/simulator_management/vec2';
+import RoadSection from '../../../containers/simulator/simulator_management/RoadSection';
+
+const { expect } = require('chai');
+const sinon = require('sinon');
+// import obj from '../../../containers/simulator/simulator_management/Vehicle';
+// import * as ts from '../../../containers/simulator/TSGeometry';
+
+describe('road intersection', () => {
+  // let intersection = new RoadIntersection(1, new Vec2(123, 321));
+
+  // let section1, section2, section3, section4;
+
+  // before(()=>{
+  //     section1 = new RoadSection(0, 1, new Vec2(120,0));
+  //     section2 = new RoadSection(1, 1, new Vec2(-120,0));
+  //     section3 = new RoadSection(2, 1, new Vec2(0,120));
+  //     section4 = new RoadSection(3, 1, new Vec2(0,-120));
+  // });
+
+  // it('get intersection id, getId', ()=>{
+  //     expect(intersection.getId()).to.equal(1);
+  // });
+
+  // it('getMapCoordinate', ()=>{
+  //     expect(intersection.getMapCoordinate()).to.equal({x:123,y:321});
+  // });
+
+  // it('addNewRoadSection', ()=>{
+  //     expect(intersection.getRoadSections().length).to.equal(0);
+
+  //     intersection.addNewRoadSection(new Vec2(120,0));
+  //     expect(intersection.getRoadSection(0)).to.deepEqual(section1);
+  //     intersection.addNewRoadSection(new Vec2(-120,0));
+  //     expect(intersection.getRoadSection(1)).to.deepEqual(section2)
+  //     intersection.addNewRoadSection(new Vec2(0, 120));
+  //     expect(intersection.getRoadSection(2)).to.deepEqual(section3);
+  //     intersection.addNewRoadSection(new Vec2(0, -120));
+  //     expect(intersection.getRoadSection(3)).to.deepEqual(section4);
+
+  //     expect(intersection.getRoadSections().length).to.equal(4);
+  // });
+
+  // it('resortRoadSections', ()=>{
+  //     intersection.resortRoadSections();
+  //     expect(intersection.getRoadSectionIndex(0)).to.equal(0);
+  //     expect(intersection.getRoadSectionIndex(2)).to.equal(1);
+  //     expect(intersection.getRoadSectionIndex(1)).to.equal(2);
+  //     expect(intersection.getRoadSectionIndex(3)).to.equal(3);
+  // });
+
+  // it('vehicles', ()=>{
+  //     expect(intersection.getVehiclesNum()).to.equal(0);
+  // });
+
+
+});
--- project/frontend/360_dashboard/src/test/simulator/simulator_management/vec2.test.spec.js
+++ project/frontend/360_dashboard/src/test/simulator/simulator_management/vec2.test.spec.js
@@ -1,41 +1,40 @@
-const assert = require('chai').assert;
-import Vec2 from "../../../containers/simulator/simulator_management/vec2";
-import { isTSAnyKeyword } from "@babel/types";
+import { isTSAnyKeyword } from '@babel/types';
+import Vec2 from '../../../containers/simulator/simulator_management/vec2';
 
-describe('Class Vec2', ()=>{
+const { assert } = require('chai');
 
-    it('constructor', ()=>{
-        let vec = new Vec2();
-        let expected = {x:0,y:0};
-        assert.deepEqual(vec, expected, "[message]");
-        vec = new Vec2(2,3);
-        expected = {x:2,y:3};
-        assert.deepEqual(vec, expected, "[message]");
-    });
+describe('Class Vec2', () => {
+  it('constructor', () => {
+    let vec = new Vec2();
+    let expected = { x: 0, y: 0 };
+    assert.deepEqual(vec, expected, '[message]');
+    vec = new Vec2(2, 3);
+    expected = { x: 2, y: 3 };
+    assert.deepEqual(vec, expected, '[message]');
+  });
 
-    it('plus', ()=>{
-        let A = new Vec2(3,1);
-        let B = new Vec2(2,3);
-        let C = A.plus(B);
-        let D = B.plus(A);
-        assert.deepEqual(C, {x:5,y:4}, "[message]");
-        assert.deepEqual(C, D, "[message]");
-    });
+  it('plus', () => {
+    const A = new Vec2(3, 1);
+    const B = new Vec2(2, 3);
+    const C = A.plus(B);
+    const D = B.plus(A);
+    assert.deepEqual(C, { x: 5, y: 4 }, '[message]');
+    assert.deepEqual(C, D, '[message]');
+  });
 
-    it('minus', ()=>{
-        let A = new Vec2(3,1);
-        let B = new Vec2(2,3);
-        let C = A.minus(B);
-        let D = B.minus(A);
-        assert.deepEqual(C, {x:1,y:-2}, "[message]");
-        assert.notDeepEqual(C, D, "[message]");
-        assert.deepEqual(D, {x:-1,y:2}, "[message]");
-    });
+  it('minus', () => {
+    const A = new Vec2(3, 1);
+    const B = new Vec2(2, 3);
+    const C = A.minus(B);
+    const D = B.minus(A);
+    assert.deepEqual(C, { x: 1, y: -2 }, '[message]');
+    assert.notDeepEqual(C, D, '[message]');
+    assert.deepEqual(D, { x: -1, y: 2 }, '[message]');
+  });
 
-    it('multiply', ()=>{
-        let A = new Vec2(3,1);
-        let B = A.multiply(4);
-        assert.deepEqual(B, {x:12,y:4}, "[message]");
-    });
-
+  it('multiply', () => {
+    const A = new Vec2(3, 1);
+    const B = A.multiply(4);
+    assert.deepEqual(B, { x: 12, y: 4 }, '[message]');
+  });
 });
