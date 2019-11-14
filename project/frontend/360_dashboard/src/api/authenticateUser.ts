943a7d691c895a197381fcf7271b2ce562aa0fb5
SaiShan <saishan.ry@gmail.com>
2019-11-14 00:22:05 -0500

    setup frontend to fetch from backend API

--- project/frontend/360_dashboard/src/api/authenticateUser.ts
+++ project/frontend/360_dashboard/src/api/authenticateUser.ts
@@ -1,21 +1,27 @@
 export interface Response {
   username: string;
-  token: string;
-  timestamp: string;
   user_id: number;
 }
 
-const APIDomain = '0.0.0.0:8000';
+const APIDomain = '127.0.0.1:8000';
 
 const authenticateUser = async (
   username: string,
   password: string,
 ): Promise<Response> => {
-  const url = `//${APIDomain}/api/user/${username}/${password}/`;
-  // const params = {
-  //   username,
-  //   password,
-  // };
+  const url = `//${APIDomain}/api/auth/login`;
+  const body = {
+    "username": username,
+    "user_id": user_id
+  }
+
+  const bodyString=JSON.stringify(body);
+
+  const settings = {
+    method: 'POST',
+    body: bodyString,
+    headers: {'Content-Type': 'application/json'},
+  }
 
   const response = await fetch(url);
   const data = (await response.json()) as Response;
