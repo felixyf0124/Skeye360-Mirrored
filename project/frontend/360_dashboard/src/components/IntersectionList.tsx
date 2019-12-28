6f7e6c2d29b30d40d9a2071459a16233bb503df2
Batoul Yehia <batoul.yehia@gmail.com>
2019-12-27 23:50:25 -0500

    #185 Created the intersection list page

--- project/frontend/360_dashboard/src/components/IntersectionList.tsx
+++ project/frontend/360_dashboard/src/components/IntersectionList.tsx
@@ -1,11 +1,13 @@
 /* eslint-disable @typescript-eslint/camelcase */
 import React from 'react';
 import SideDrawer from './SideDrawer';
+import IntersectionTable from './IntersectionTable';
 
 // const headerTitle = "Intersection List";
 const IntersectionList = (): any => (
-  <div>
+  <div style={{ display: 'flex'}}>
     <SideDrawer />
+    <IntersectionTable /> 
   </div>
 );
 
