fef7f814f177ac38ee866e6bfac81d7df1719eb8
Batoul Yehia <batoul.yehia@gmail.com>
2020-01-07 17:00:54 -0500

    #212 Creates conversion function from kelvin to celsius and displays the correct temperature.

--- project/frontend/360_dashboard/src/components/Weather.tsx
+++ project/frontend/360_dashboard/src/components/Weather.tsx
@@ -1,34 +1,68 @@
 import React, { Component } from 'react';
 const API_KEY="08e715234386b00a47120f045bc02858";
-const city = '';
+const city = 'Montreal'; //for now just hardcode, fetch city later from DB just to make sure it works 
 const API_CALL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`;
 
-interface StateProps{
-    currentWeather: string
-    getCurrentWeather: any
-}
-interface DispatchProps{
-    currentWeather: string
-}
+//Reference: https://reactjs.org/docs/faq-ajax.html
+//On how to make API calls in ReactJS
+//Temperature is actually given in Kelvin, so change that! 
 
-
-class Weather extends React.Component<(StateProps & DispatchProps) | any> {
-    public componentDidMount(): void {
+const convertCelsius = (kelvinWeather: any): any => {
+    return Number(kelvinWeather - 273).toFixed();
+};
+class Weather extends React.Component<{},{error: any, isLoaded: boolean, weatherIcon: string, temperature: string, tempDescription: string}> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            weatherIcon: "",
+            temperature: "",
+            tempDescription: ""
+        };
+    }
+    componentDidMount(): void {
       // eslint-disable-next-line no-shadow
         fetch(API_CALL)
         .then(results => {
             return results.json()
-        }).then(data =>{
-            let currWeather = data.results.main;
-        });
-        //this.setState
-    }
-    public render(): JSX.Element {
-
-        return (
-            <div>
-
-            </div>
+            }).then(data =>{
+                this.setState({
+                    isLoaded: true,
+                    weatherIcon: data.weather.icon,
+                    temperature: data.main.temp,
+                    tempDescription: data.weather.description,
+                })
+            },
+            (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error
+                });
+            }
+        
         );
     }
+    
+    render(): JSX.Element {
+        const { error, isLoaded, weatherIcon, temperature, tempDescription } = this.state;
+        const tempCelsius = convertCelsius(temperature);
+        if(error){
+            return<div>Error: {error.message}</div>
+        }
+        else if (!isLoaded){
+            return<div>Loading...</div>
+        }
+        else{
+            return (
+                <div>
+                    <div>Icon:{weatherIcon}</div>
+                    <div>Temperature: {tempCelsius}&deg;C</div>
+                    <div>Description:{tempDescription}</div>
+              
+                </div>
+            );
+        }
+    }
 }
+export default Weather; 
