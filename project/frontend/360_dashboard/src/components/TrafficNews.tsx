b1a5700dd1e6a7f62b11f142bb466db7359617a1
Batoul Yehia <batoul.yehia@gmail.com>
2020-01-10 21:29:48 -0500

    #213 Retrieves the correct information, and styles it

--- project/frontend/360_dashboard/src/components/TrafficNews.tsx
+++ project/frontend/360_dashboard/src/components/TrafficNews.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Typography from '@material-ui/core/Typography';
+import styled from 'styled-components';
+import Paper from '@material-ui/core/Paper'
 
 //MapQuest API is used to retrieve traffic news
 //https://developer.mapquest.com/documentation/traffic-api/incidents/get/
@@ -8,16 +12,6 @@ import React from 'react';
 //Parameters required: API key, boundingBox
 //Bounding Box for Montreal found at: https://boundingbox.klokantech.com/
 
-//O34: $$dW0735800$$eW0732800$$fN0454200$$gN0452400
-//255: $$c(W 73°58'00"--W 73°28'00"/N 45°42'00"--N 45°24'00")
-
-//http://www.mapquestapi.com/traffic/v2/incidents?key=KEY&boundingBox=39.95,-105.25,39.52,-104.71&filters=construction,incidents
-
-//<bounding><westbc>-73.97290173</westbc><eastbc>-73.47429525</eastbc><northbc>45.7047897</northbc><southbc>45.41007553</southbc></bounding>
-
-//[[[-73.97290173,45.41007553],[-73.47429525,45.41007553],[-73.47429525,45.7047897],[-73.97290173,45.7047897],[-73.97290173,45.41007553]]]
-//westlimit=-73.972902; southlimit=45.410076; eastlimit=-73.474295; northlimit=45.70479 //I GUESS USE THIS???
-
 //Bounding Box: Top right and bottom left areas corners of an area
 /* Incident Types:
     1 = Construction
@@ -26,11 +20,10 @@ import React from 'react';
     4 = Incident/accident
 */
 
+
 const API_KEY = '24jtUJNMCXQg4pLgMchaC7p6Flihs7wO';
 
 const BOUNDING_BOX = '45.7047897,-73.47429525,45.41007553,-73.97290173';
-const EXAMPLE = '39.95,-105.25,39.52,-104.71';
-const FILTERS = 'congestion';
 const API_CALL = `http://www.mapquestapi.com/traffic/v2/incidents?key=${API_KEY}&boundingBox=${BOUNDING_BOX}`;
 
 interface StateProps {
@@ -39,15 +32,20 @@ interface StateProps {
     incidents: any;
 }
 
-interface IncidentProps {
-    id: number;
-    type: number;
-    severity: number;
-    startTime: string;
-    endTime: string;
-    shortDesc: string;
-    fullDesc: string;
-} 
+const OuterContainer = styled.div`
+    background-color: white;
+    width: 55rem;
+    overflow: scroll;
+    padding: 1rem;
+    height: 30rem;
+`;
+
+const OuterDiv = styled.div`
+    color: white; 
+    margin: 5rem;
+    display: flex;
+    flex-direction: column; 
+`;
 
 class TrafficNews extends React.Component<{}, StateProps>{
     constructor(props: any){
@@ -79,16 +77,51 @@ class TrafficNews extends React.Component<{}, StateProps>{
     render(): JSX.Element {
         const { error, isLoaded, incidents } = this.state; 
         console.log(incidents);
-
-        return(
-            <div>
-            {incidents.map((incident: { id: string | number | undefined; type: React.ReactNode; }) => (
-              <li key={incident.id}>
-                {incident.type}
-              </li>
-            ))}
-            </div>
-        )
+        if(error){
+            return(
+                <div>
+                    Error
+                </div>
+            );
+        }
+        else if(!isLoaded){
+            return(
+                <CircularProgress />
+            );
+        }
+        else{
+            return(
+                <OuterDiv>
+                <Typography variant="h4">
+                    Traffic News
+                </Typography>
+                <OuterContainer>
+                    {incidents.map((incident: { id: string | number | undefined; type: any; shortDesc: string; fullDesc: string; severity: any; startTime: any; endTime: any;}) => (
+                        <Paper elevation={3} key={incident.id} style={{marginBottom: '2rem', padding:'1rem'}}>
+                            <Typography variant="h6">
+                                {(() => {
+                                    switch(incident.type){
+                                        case 1: return "Construction";
+                                        case 2: return "Event";
+                                        case 3: return "Congestion/Flow";
+                                        case 4: return "Incident/Accident";
+                                    }
+                                })()}
+                            </Typography>
+                            <Typography variant="subtitle1">
+                                {incident.shortDesc}
+                            </Typography>
+                            {incident.fullDesc}
+                            <br/><br/>
+                            <b>Severity:</b> {incident.severity} <br />
+                            <b>Start Time:</b> {incident.startTime} <br />
+                            <b>End Time:</b> {incident.endTime}
+                        </Paper>
+                    ))}
+                </OuterContainer>
+                </OuterDiv>
+            )
+        }
     }
 }
 
