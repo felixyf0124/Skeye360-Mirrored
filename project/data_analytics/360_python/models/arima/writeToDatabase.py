2396a130648a0c56ab04dcb603455b5ada7b51a3
lokevin88 <lo_kevin@outlook.com>
2020-01-11 02:01:41 -0500

    #220 Made it as a class

--- project/data_analytics/360_python/models/arima/writeToDatabase.py
+++ project/data_analytics/360_python/models/arima/writeToDatabase.py
@@ -1,34 +1,38 @@
-from dbConnection import client
+from dbConnection import dbConnection
 
-# CONTINUE WORKING ON CREATING THE DB AND COLLECTION
-# THEN INSERT ALL PREDICTIONS AND READ IT
+class writeToDatabase:
 
-# To connect to the database
-db = client['mongoDatabase']
-# Collection are table in SQL
-collection = db['Prediction']
+    def connection(self):
+        # To connect to the database
+        dbConnect = dbConnection()
+        client = dbConnect.connect()
+        db = client['mongoDatabase']
+        # Collection are table in SQL
+        collection = db['Prediction']
+        return collection
 
-def write(direction, predictions, time, modelType):
-    # If ran too many times and you want to delete all records (or the whole collection), uncomment the following code
-    # collection.remove()
+    def write(self, direction, predictions, time, modelType):
+        collection = self.connection()
+        # If ran too many times and you want to delete all records (or the whole collection), uncomment the following code
+        # collection.remove()
 
-    # START of writing to MongoDB
-    # For loop to insert into MongoDB
-    for index in range(len(predictions)):
-        collection.insert_one({"directions":direction, "count":predictions[index], "time":time[index], "type":modelType})
-    # END of writing to MongoDB
+        # START of writing to MongoDB
+        # For loop to insert into MongoDB
+        for index in range(len(predictions)):
+            collection.insert_one({"directions":direction, "count":predictions[index], "time":time[index], "type":modelType})
+        # END of writing to MongoDB
 
-def read(collectionName):
-    # START of retrieving data from MongoDB
-    print('----------------------------')
-    collectionData = db[collectionName]
-    resultsRaw = collectionData.find({})
-    print(resultsRaw.count())
-    # For loop to read in row
-    for row in resultsRaw:
-        resultDirection = str(row['directions'])
-        resultCount = str(row['count'])
-        resultTime = str(row['time'])
-        resultType = str(row['type'])
-        print(resultDirection + " | " + resultCount + " | " + resultTime + " | " + resultType)
-    # END of retrieving data from MongoDB
+    def read(self):
+        # START of retrieving data from MongoDB
+        print('----------------------------')
+        collection = self.connection()
+        resultsRaw = collection.find({})
+        print(resultsRaw.count())
+        # For loop to read in row
+        for row in resultsRaw:
+            resultDirection = str(row['directions'])
+            resultCount = str(row['count'])
+            resultTime = str(row['time'])
+            resultType = str(row['type'])
+            print(resultDirection + " | " + resultCount + " | " + resultTime + " | " + resultType)
+        # END of retrieving data from MongoDB
