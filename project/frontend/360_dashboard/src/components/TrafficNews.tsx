65a615a76df7d4dacd6ab9cd45586e71ea780269
Batoul Yehia <batoul.yehia@gmail.com>
2020-01-12 12:42:22 -0500

    #213 Cleans up the code and fixes lint errors

--- project/frontend/360_dashboard/src/components/TrafficNews.tsx
+++ project/frontend/360_dashboard/src/components/TrafficNews.tsx
@@ -2,29 +2,25 @@ import React from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Typography from '@material-ui/core/Typography';
 import styled from 'styled-components';
-import Paper from '@material-ui/core/Paper'
-import EventIcon from '@material-ui/icons/Event';
-import ReportProblemIcon from '@material-ui/icons/ReportProblem';
-import TimeToLeaveIcon from '@material-ui/icons/TimeToLeave'; 
-import TrafficIcon from '@material-ui/icons/Traffic';
+
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import { SKEYE_BLUE, SKEYE_GREEN, SKEYE_GREY, SKEYE_WHITE } from '../css/custom';
+import { SKEYE_WHITE } from '../css/custom';
 
-//MapQuest API is used to retrieve traffic news
-//https://developer.mapquest.com/documentation/traffic-api/incidents/get/
+// MapQuest API is used to retrieve traffic news
+// https://developer.mapquest.com/documentation/traffic-api/incidents/get/
 
-//Resource URL: http://www.mapquestapi.com/traffic/v2/incidents
+// Resource URL: http://www.mapquestapi.com/traffic/v2/incidents
 
-//Parameters required: API key, boundingBox
-//Bounding Box for Montreal found at: https://boundingbox.klokantech.com/
+// Parameters required: API key, boundingBox
+// Bounding Box for Montreal found at: https://boundingbox.klokantech.com/
 
-//Bounding Box: Top right and bottom left areas corners of an area
+// Bounding Box: Top right and bottom left areas corners of an area
 /* Incident Types:
-    1 = Construction
-    2 = Event
-    3 = Congestion/Flow
-    4 = Incident/accident
+  1 = Construction
+  2 = Event
+  3 = Congestion/Flow
+  4 = Incident/accident
 */
 
 
@@ -34,104 +30,115 @@ const BOUNDING_BOX = '45.7047897,-73.47429525,45.41007553,-73.97290173';
 const API_CALL = `http://www.mapquestapi.com/traffic/v2/incidents?key=${API_KEY}&boundingBox=${BOUNDING_BOX}`;
 
 interface StateProps {
-    error: any;
-    isLoaded: boolean;
-    incidents: any;
+  error: any;
+  isLoaded: boolean;
+  incidents: any;
 }
 
 const OuterContainer = styled.div`
-    width: 50rem;
-    overflow: scroll;
-    padding: 1rem;
-    height: 40rem;
-    overflow-x: hidden;
+  width: 50rem;
+  overflow: scroll;
+  padding: 1rem;
+  height: 40rem;
+  overflow-x: hidden;
 `;
 
 const OuterDiv = styled.div`
-    color: ${SKEYE_WHITE};
-    margin: 5rem;
-    display: flex;
-    flex-direction: column; 
+  color: ${SKEYE_WHITE};
+  margin: 5rem;
+  display: flex;
+  flex-direction: column; 
 `;
 
-class TrafficNews extends React.Component<{}, StateProps>{
-    constructor(props: any){
-        super(props);
-        this.state = {
-            error: null,
-            isLoaded: false,
-            incidents: []
-        };
-    }
+class TrafficNews extends React.Component<{}, StateProps> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      incidents: [],
+    };
+  }
 
-    componentDidMount(): void { 
-        //eslint-disable-next-line no-shadow
-        fetch(API_CALL)
-        .then((results) => results.json()).then((data) => {
+  componentDidMount(): void {
+    // eslint-disable-next-line no-shadow
+    fetch(API_CALL)
+      .then((results) => results.json()).then((data) => {
         this.setState({
-            isLoaded: true,
-            incidents: data.incidents
+          isLoaded: true,
+          incidents: data.incidents,
         });
-        },
-        (error) => {
+      },
+      (error) => {
         this.setState({
-            isLoaded: true,
-            error,
-            });
+          isLoaded: true,
+          error,
         });
-    }
+      });
+  }
 
-    render(): JSX.Element {
-        const { error, isLoaded, incidents } = this.state; 
-        console.log(incidents);
-        if(error){
-            return(
-                <div>
+  render(): JSX.Element {
+    const { error, isLoaded, incidents } = this.state;
+    if (error) {
+      return (
+        <div>
                     Error
-                </div>
-            );
-        }
-        else if(!isLoaded){
-            return(
-                <CircularProgress />
-            );
-        }
-        else{
-            return(
-                <OuterDiv>
-                <OuterContainer>
-                    {incidents.map((incident: { id: string | number | undefined; type: any; shortDesc: string; fullDesc: string; severity: any; startTime: any; endTime: any;}) => (
-                        <Card style={{margin:'1rem'}}>
-                            <CardContent>
-                                <Typography variant="h5">
-                                    Traffic Type: &nbsp;
-                                    {(() => {
-                                        switch(incident.type){
-                                            case 1: return "Construction";
-                                            case 2: return "Event";
-                                            case 3: return "Congestion/Flow";
-                                            case 4: return "Incident/Accident";
-                                        }
-                                    })()}
-                                </Typography>
-                                <Typography variant="h6">
-                                    {incident.shortDesc}
-                                </Typography>
-                                <Typography variant="subtitle1">
-                                    {incident.fullDesc}
-                                    <br/><br/>
-                                    <b>Severity:</b> {incident.severity} <br />
-                                    <b>Start Time:</b> {incident.startTime} <br />
-                                    <b>End Time:</b> {incident.endTime}
-                                </Typography>
-                                </CardContent>
-                        </Card>
-                    ))}
-                </OuterContainer>
-                </OuterDiv>
-            )
-        }
+        </div>
+      );
     }
+    if (!isLoaded) {
+      return (
+        <CircularProgress />
+      );
+    }
+    /* eslint-disable max-len */
+    /* eslint-disable @typescript-eslint/explicit-function-return-type */
+    return (
+      <OuterDiv>
+        <OuterContainer>
+          {incidents.map((incident: { id: string | number | undefined; type: any; shortDesc: string; fullDesc: string; severity: any; startTime: any; endTime: any}) => (
+            <Card style={{ margin: '1rem' }} key={incident.id}>
+              <CardContent>
+                <Typography variant="h5">
+                                    Traffic Type: &nbsp;
+                  {(() => {
+                    switch (incident.type) {
+                      case 1: return 'Construction';
+                      case 2: return 'Event';
+                      case 3: return 'Congestion/Flow';
+                      case 4: return 'Incident/Accident';
+                      default: return 'Traffic';
+                    }
+                  })()}
+                </Typography>
+                <Typography variant="h6">
+                  {incident.shortDesc}
+                </Typography>
+                <Typography variant="subtitle1">
+                  {incident.fullDesc}
+                  <br />
+                  <br />
+                  <b>Severity:</b>
+                  {' '}
+                  {incident.severity}
+                  {' '}
+                  <br />
+                  <b>Start Time:</b>
+                  {' '}
+                  {incident.startTime}
+                  {' '}
+                  <br />
+                  <b>End Time:</b>
+                  {' '}
+                  {incident.endTime}
+                </Typography>
+              </CardContent>
+            </Card>
+          ))}
+        </OuterContainer>
+      </OuterDiv>
+    );
+  }
 }
 
-export default TrafficNews; 
+export default TrafficNews;
