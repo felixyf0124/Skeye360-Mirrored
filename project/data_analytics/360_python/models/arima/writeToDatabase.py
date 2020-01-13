bd8dfda7941217021652e5c8dec5f98b50826705
lokevin88 <lo_kevin@outlook.com>
2020-01-12 22:46:40 -0500

    #220 Updated code based on REST

--- project/data_analytics/360_python/models/arima/writeToDatabase.py
+++ project/data_analytics/360_python/models/arima/writeToDatabase.py
@@ -1,4 +1,5 @@
 from dbConnection import dbConnection
+import pandas as panda
 
 class writeToDatabase:
 
@@ -6,20 +7,27 @@ class writeToDatabase:
         # To connect to the database
         dbConnect = dbConnection()
         client = dbConnect.connect()
-        db = client['mongoDatabase']
+        db = client['360backend']
+        if db is None:
+            print("db connection failed")
+        else:
+            print("db is connected")
         # Collection are table in SQL
-        collection = db['Prediction']
+        collection = db['djangosite_api_count']
         return collection
 
     def write(self, direction, predictions, time, modelType):
         collection = self.connection()
         # If ran too many times and you want to delete all records (or the whole collection), uncomment the following code
-        # collection.remove()
+        collection.remove()
 
         # START of writing to MongoDB
+        # The following code is to get the latest id inserted
+        resultsRaw = collection.find({})
+        lastId = resultsRaw.count()
         # For loop to insert into MongoDB
         for index in range(len(predictions)):
-            collection.insert_one({"directions":direction, "count":predictions[index], "time":time[index], "type":modelType})
+            collection.insert_one({"id": lastId + index, "count_direction":direction, "count":predictions[index], "time":time[index], "count_type":modelType})
         # END of writing to MongoDB
 
     def read(self):
@@ -27,12 +35,15 @@ class writeToDatabase:
         print('----------------------------')
         collection = self.connection()
         resultsRaw = collection.find({})
-        print(resultsRaw.count())
+        
+        # Uncomment the following line of codes to see the number of records
+        # print(resultsRaw.count())
+        
         # For loop to read in row
         for row in resultsRaw:
-            resultDirection = str(row['directions'])
+            resultDirection = str(row["count_direction"])
             resultCount = str(row['count'])
             resultTime = str(row['time'])
-            resultType = str(row['type'])
+            resultType = str(row['count_type'])
             print(resultDirection + " | " + resultCount + " | " + resultTime + " | " + resultType)
         # END of retrieving data from MongoDB
