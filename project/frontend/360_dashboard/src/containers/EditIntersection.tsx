4e4bd579a5e4f7ef6c26d87832f3696de6ab0106
Kevin L <Lin_Kevin_1995@hotmail.com>
2020-01-17 01:50:39 -0500

    #250 Camera being undefined shouldn't crash the website now.

--- project/frontend/360_dashboard/src/containers/EditIntersection.tsx
+++ project/frontend/360_dashboard/src/containers/EditIntersection.tsx
@@ -93,14 +93,18 @@ class EditIntersection extends React.Component<StateProps & DispatchProps> {
           <HorizontalFlexBox>
             {intersection_name === '' ? <div /> : <EditIntersectionForm />}
             <VerticalFlexBox>
-              {cameras.map((camera: cameraResponse) => (
-                <EditCameraForm
-                  key={camera.id}
-                  id={String(camera.id)}
-                  camera_url={camera.camera_url}
-                  intersection_id={String(camera.intersection_id)}
-                />
-              ))}
+              {cameras === undefined ? (
+                <div />
+              ) : (
+                cameras.map((camera: cameraResponse) => (
+                  <EditCameraForm
+                    key={camera.id}
+                    id={String(camera.id)}
+                    camera_url={camera.camera_url}
+                    intersection_id={String(camera.intersection_id)}
+                  />
+                ))
+              )}
               <AddCameraForm intersection_id={Number(intersection_id)} />
             </VerticalFlexBox>
           </HorizontalFlexBox>
