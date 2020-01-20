d35826e2fb1c10942cf9846f6970ff187490ac13
Batoul Yehia <batoul.yehia@gmail.com>
2020-01-20 11:53:52 -0500

    #243 ABle to retrieve count values from db

--- project/frontend/360_dashboard/src/components/DisplayMovAVG.tsx
+++ project/frontend/360_dashboard/src/components/DisplayMovAVG.tsx
@@ -1,12 +1,44 @@
 import React from 'react';
-import { STATE as countState } from '../contexts/countTime';
+import { getCount, GetCountAction } from '../contexts/countTime';
+import SideDrawer from './SideDrawer';
+import { Response as CountResponse } from '../api/countTime';
+import { RootState } from '../reducers/rootReducer';
+import { connect } from 'react-redux';
+import { push } from 'connected-react-router';
 
-const DisplayCount = (count: countState): JSX.Element => {
-    console.log(count);
-    console.log("Test");
-    return(
-        <div></div>
-    )
+interface StateProps {
+    counts: CountResponse;
 }
 
-export default DisplayCount; 
+interface DispatchProps {
+    getCount(): GetCountAction;
+    historyPush: (url: string) => void;
+}
+class DisplayCount extends React.Component<StateProps & DispatchProps> {
+    public componentDidMount(): void {
+        getCount();
+    }
+   public render(): JSX.Element {
+    const { getCount } = this.props;
+    getCount();
+
+    console.log(getCount);
+        return(
+            <div style={{margin: '50vh', backgroundColor: 'pink'}}>
+                H-HEWWO???? uwu
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state: RootState): StateProps => ({
+    ...state,
+    counts: state.countTime,
+});
+
+const mapDispatchToProps: DispatchProps = {
+    getCount,
+    historyPush: push,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DisplayCount);
