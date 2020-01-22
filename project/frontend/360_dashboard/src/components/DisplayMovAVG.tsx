9df48f55767da6b6ea825e677f45b6d42d31b54b
Batoul Yehia <batoul.yehia@gmail.com>
2020-01-22 13:12:08 -0500

    #243 Created a method that retrieves same date from last year and a method that sorts counts into NS direction.

--- project/frontend/360_dashboard/src/components/DisplayMovAVG.tsx
+++ project/frontend/360_dashboard/src/components/DisplayMovAVG.tsx
@@ -9,8 +9,12 @@ import { STATE as countState } from '../contexts/countTime';
 
 interface StateProps {
   countAvg: countState;
-}
+  intersection_id: string;
 
+}
+interface SomeProps{
+ // intersection_id: string;
+}
 interface DispatchProps {
   getCountMAvg(): GetCountMAvgAction;
 }
@@ -32,22 +36,46 @@ class DisplayCount extends React.Component<StateProps & DispatchProps> {
 }
 const mapStateToProps = (state: RootState): StateProps => ({
   countAvg: state.countTime,
+  intersection_id: state.router.location.pathname.substring(
+    state.router.location.pathname.lastIndexOf('/') + 1,
+  )
 });
 
 const mapDispatchToProps: DispatchProps = {
   getCountMAvg,
 };
 
-
-function getCountsLastYr(){
-  //var lastYear: CountResponse[] = [];
+//Function that generates the exact date last year to retrieve based on today's date
+//This gets saved into a STATE that will be passed to the GET Request.
+function getDateLastYear(){
+  //Create a new date, today
   var today = new Date();
-  const currentMonth = today.getMonth();
-  const currentYear = today.getFullYear();
-  const currentDay = today.getDate();
+  //Convert to ISO
+  const todayToISO = today.toISOString();
+  //Retrieve the current year
+  var thisYear = todayToISO.substring(0,3);
+  var thisYearInt = parseInt(thisYear);
+  const lastYear = thisYearInt - 1;
+  const lastYearString = lastYear.toString();
 
-  const lastYear = currentYear - 1;
-  var lastYrDate = new Date(currentYear,currentMonth,currentDay);
+  //Retrieve the date (month and day)
+  var monthDay = todayToISO.substring(4,9);
+  var lastYearDate = lastYearString + monthDay; 
+
+  return lastYearDate; 
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(DisplayCount);
+function mapIntoNS(counts: countState[]){
+  const x: any[] = [];
+  const y: number[] = [];
+  counts.map((count)=> {
+    if(count.count_direction === 'NS'){
+      x.push(count.time);
+      y.push(count.count);
+    }
+  });
+  return [x,y];
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(DisplayCount);;
