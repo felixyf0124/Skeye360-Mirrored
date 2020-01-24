72b0ddfafd54895ed0d1a090d844b4cb676cbcce
g82005 <davidseechan@gmail.com>
2020-01-24 03:37:17 -0500

    fixing typo in urls.py #263

--- project/backend/backend_django/camera/camera/urls.py
+++ project/backend/backend_django/camera/camera/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     url('admin/', admin.site.urls),
     url('cam/', recognition_views.cam),
     url('coord/',recognition_views.send_json),
-    url('los/',recognition_views.get_level_of_service)
+    url('los/',recognition_views.get_level_of_service),
     url('lights/',recognition_views.get_light_signals)
 ]
