8d428da421cdf380c9bed1b2c5f48143cea99131
Batoul Yehia <batoul.yehia@gmail.com>
2020-01-26 16:23:19 -0500

    #243 Refactored the function that sorts data by direction and removed useless code

--- project/frontend/360_dashboard/src/components/DisplayMovAVG.tsx
+++ project/frontend/360_dashboard/src/components/DisplayMovAVG.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { RootState } from '../reducers/rootReducer';
 import { connect } from 'react-redux';
 import { getCountMAvg, GetCountMAvgAction } from '../contexts/countTime';
-import { STATE as countState } from '../contexts/countTime';
 import { Response as countResponse } from '../api/countTime';
 
 interface StateProps {
@@ -29,8 +28,11 @@ class DisplayCount extends React.Component<StateProps & DispatchProps> {
   public render(): JSX.Element {
     const { countAvg } = this.props;
     const directions = mapByDirection(countAvg);
-    //console.log(directions[1]);
-
+    for(var i=0; i<directions.length; i++){
+      if(directions[i] !== undefined){
+        console.log(directions[i]);
+      }
+    }
     return(
       <div style={{margin: '50vh', color: 'pink'}}>
       </div>
@@ -79,152 +81,123 @@ function getDateLastYear(){
   return [getLastYearDateFormat, getLastYearNextDayFormat];
 }
 
-//Function that retrieves time strings and associates them with an hour of the day
-function mapHours(times: string[]){
-  const newX: any[] = [];
-  var currentTime;
-  var timeToInt;
-  times.map((time) => {
-    currentTime = time.substring(11,12);
-    timeToInt = parseInt(currentTime);
-    newX.push(time)
-  });
-}
-
 //Function that places every count response in a different array based on its direction
+/* eslint-disable @typescript-eslint/no-array-constructor */
 function mapByDirection(counts: countResponse){
-  const xns: any[] = [];
-  const yns: number[] = [];
+  const ns = {
+    count: Array(),
+    direction: 'ns',
+    time: Array(),
+  };
+  const sn = {
+    count: Array(),
+    direction: 'sn',
+    time: Array(),
+  }
+  const en = {
+    count: Array(),
+    direction: 'en',
+    time: Array(),
+  };
+  const es = {
+    count: Array(),
+    direction: 'es',
+    time: Array(),
+  };
+  const ew = {
+    count: Array(),
+    direction: 'ew',
+    time: Array(),
+  };
+  const ne = {
+    count: Array(),
+    direction: 'ne',
+    time: Array(),
+  };
+  const nw = {
+    count: Array(),
+    direction: 'nw',
+    time: Array(),
+  };
+  const se = {
+    count: Array(),
+    direction: 'se',
+    time: Array(),
+  };
+  const sw = {
+    count: Array(),
+    direction: 'sw',
+    time: Array(),
+  };
+  const we = {
+    count: Array(),
+    direction: 'we',
+    time: Array(),
+  };
+  const wn = {
+    count: Array(),
+    direction: 'wn',
+    time: Array(),
+  };
+  const ws = {
+    count: Array(),
+    direction: 'ws',
+    time: Array(),
+  };
 
-  const xsn: any[] = [];
-  const ysn: number[] = [];
-
-  const xen: any[] = [];
-  const yen: number[] = [];
-  
-  const xes: any[] = [];
-  const yes: number[] = [];
-
-  const xew: any[] = [];
-  const yew: number[] = [];
-
-  const xne: any[] = [];
-  const yne: number[] = [];
-
-  const xnw: any[] = [];
-  const ynw: number[] = [];
-
-  const xse: any[] = [];
-  const yse: number[] = [];
-
-  const xsw: any[] = [];
-  const ysw: number[] = [];
-
-  const xwe: any[] = [];
-  const ywe: number[] = [];
-
-  const xwn: any[] = [];
-  const ywn: number[] = [];
-
-  const xws: any[] = [];
-  const yws: number[] = [];
-
+  //Iterating through the count responses 
   Object.values(counts).forEach(value => {
-    if(value.count_direction === 'ns'){
-      xns.push(value.time);
-      yns.push(value.count);
-    }
-    if(value.count_direction === 'sn'){
-      xsn.push(value.time);
-      ysn.push(value.count);
-    }
-    if(value.count_direction === 'en'){
-      xen.push(value.time);
-      yen.push(value.count);
-    }
-    if(value.count_direction === 'es'){
-      xes.push(value.time);
-      yes.push(value.count);
-    }
-    if(value.count_direction === 'ew'){
-      xew.push(value.time);
-      yew.push(value.count);
-    }
-    if(value.count_direction === 'ne'){
-      xne.push(value.time);
-      yne.push(value.count);
-    }
-    if(value.count_direction === 'nw'){
-      xnw.push(value.time);
-      ynw.push(value.count);
-    }
-    if(value.count_direction === 'se'){
-      xse.push(value.time);
-      yse.push(value.count);
-    }
-    if(value.count_direction === 'sw'){
-      xsw.push(value.time);
-      ysw.push(value.count);
-    }
-    if(value.count_direction === 'we'){
-      xwe.push(value.time);
-      ywe.push(value.count);
-    }
-    if(value.count_direction === 'wn'){
-      xwn.push(value.time);
-      ywn.push(value.count);
-    }
-    if(value.count_direction === 'ws'){
-      xws.push(value.time);
-      yws.push(value.count);
+    switch(value.count_direction){
+      case 'ns': 
+        ns.count.push(value.count);
+        ns.time.push(value.time.substring(11,12));
+      break;
+      case 'sn':
+        sn.count.push(value.count);
+        sn.time.push(value.time.substring(11,12));
+      break;
+      case 'en': 
+        en.count.push(value.count);
+        en.time.push(value.time.substring(11,12));
+      break;
+      case 'es':
+        es.count.push(value.count);
+        es.time.push(value.time.substring(11,12));
+      break;
+      case 'ew':
+        ew.count.push(value.count);
+        ew.time.push(value.time.substring(11,12));
+      break;
+      case 'ne':
+        ne.count.push(value.count);
+        ne.time.push(value.time.substring(11,12));
+      break;
+      case 'nw':
+        nw.count.push(value.count);
+        nw.time.push(value.time.substring(11,12));
+      break;
+      case 'se':
+        se.count.push(value.count);
+        se.time.push(value.time.substring(11,12));
+      break;
+      case 'sw':
+        sw.count.push(value.count);
+        sw.time.push(value.time.substring(11,12));
+      break;
+      case 'we':
+        we.count.push(value.count);
+        we.time.push(value.time.substring(11,12));
+      break;
+      case 'wn':
+        wn.count.push(value.count);
+        wn.time.push(value.time.substring(11,12));
+      break;
+      case 'ws':
+        ws.count.push(value.count);
+        ws.time.push(value.time.substring(11,12));
+      break;
     }
   }); 
-  
-  return [
-    {xns, yns},
-    {xsn, ysn},
-    {xen, yen},
-    {xes, yes},
-    {xew, yew},
-    {xne, yne},
-    {xnw, ynw},
-    {xse, yse},
-    {xsw, ysw},
-    {xwe, ywe},
-    {xwn, ywn},
-    {xws, yws}
-  ];
+  return [ns, sn, en, es, ew, ne, nw, se, sw, we, wn, ws];
 }
-
-//Function that sorts count responses based on time, interval of 24 hours
-//Sorting from smallest to largest
-//Sort from key-value pairs
-//https://stackoverflow.com/questions/14208651/javascript-sort-key-value-pair-object-based-on-value
-//https://stackoverflow.com/questions/5467129/sort-javascript-object-by-key
-function mapIntoKeyPairValues(counts: countState[]){
-  var keyValuePairArray: any[] = [];
-  var keyTimes: any[] = [];
-  var sortedPair: any[] = [];
-
-  //iterate through the count responses to create an array with key-pair values that are time and counts respectively
-  //Obtain the specific hour in the time and use it as the key
-  counts.map((count) =>{
-    keyValuePairArray.push({
-      key: parseInt(count.time.substring(11,12)),
-      value: count.count,
-    });
-    //push the keys in another array for sorting purposes
-    keyTimes.push(parseInt(count.time.substring(11,12)));
-  });
-
-  //sort the keys
-  keyTimes.sort();
-
-  //sort the key-value array based on the sorted keys array
-  keyTimes.map((keyTime) => {
-    sortedPair.push(keyValuePairArray[keyTime]);
-  });
-  return sortedPair;
-}
-
 export default connect(mapStateToProps, mapDispatchToProps)(DisplayCount);
