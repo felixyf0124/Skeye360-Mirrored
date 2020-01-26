365a88e219156d68541591249edcd38ef4bfc99b
Chen <43104118+g82005@users.noreply.github.com>
2020-01-26 03:14:26 -0500

    fixing typo in tests #263
    fixing typo in tests #263

--- project/backend/backend_django/camera/recognition/test/test_functions.py
+++ project/backend/backend_django/camera/recognition/test/test_functions.py
@@ -3,7 +3,7 @@ import sys
 import os
 import io
 import unittest
-import mongomock`
+import mongomock
 from unittest.mock import MagicMock, patch
 from ..direction import Direction
 from ..counter import Counter
