21ef5b34f396fe6c51c1917557182e8d915fd481
SaiShan <saishan.ry@gmail.com>
2020-01-27 01:46:24 -0500

    #261 added test

--- project/backend/backend_django/camera/real_time/realtime.py
+++ project/backend/backend_django/camera/real_time/realtime.py
@@ -162,3 +162,145 @@ class Realtime:
         self.timers['east-west'] = eastWestTimer
         self.timers['left'] = leftTurnTimer
         
+    # For testing purposed
+    def det_timers_test(self, data):
+
+        #  Extracting traffic data from csv file.
+        raw_data = data
+        
+        # Convert csv file data column to datetime index.
+        raw_data['datetime'] = panda.to_datetime(raw_data['datetime'])
+        
+        # Sorting time (ascending) and resetting index.
+        raw_data = raw_data.sort_values(['datetime'])
+        raw_data = raw_data.reset_index(drop=True)
+
+        # Adding new column with time im milliseconds.
+        raw_data['milli_time'] = (((((raw_data['datetime'].dt.hour * 60) + raw_data['datetime'].dt.minute) * 60) + raw_data['datetime'].dt.second) * 1000)
+
+        # Get current time.
+        t_sys = int(self.get_milli_time())
+
+        # Get data period.
+        period = int(raw_data.tail(1)['milli_time']) - int(raw_data.head(1)['milli_time'])
+
+        # Set matching time (timeline for the loop).
+        currentTime = np.mod(t_sys, period)
+        
+        # Add loop_time to match current time each call.
+        raw_data = self.add_loop_time(raw_data)
+
+        # Set start and end range time to run algorithm.
+        self.start = np.mod((np.mod(t_sys, period) - (20*1000) + period), period)
+        self.end = currentTime
+
+        # Data selection based on time. 
+        raw_data = raw_data.loc[(raw_data['loop_time'] >= self.start) & (raw_data['loop_time'] <= self.end)]
+
+        # Separating traffic data by directions.
+        southToSouth = raw_data.loc[(raw_data['from'] == "south") & (raw_data['to'] == "south")]
+        southToNorth = raw_data.loc[(raw_data['from'] == "south") & (raw_data['to'] == "north")]
+        southToEast = raw_data.loc[(raw_data['from'] == "south") & (raw_data['to'] == "east")]
+        southToWest = raw_data.loc[(raw_data['from'] == "south") & (raw_data['to'] == "west")]
+
+        northToNorth = raw_data.loc[(raw_data['from'] == "north") & (raw_data['to'] == "north")]
+        northToSouth = raw_data.loc[(raw_data['from'] == "north") & (raw_data['to'] == "south")]
+        northToEast = raw_data.loc[(raw_data['from'] == "north") & (raw_data['to'] == "east")]
+        northToWest = raw_data.loc[(raw_data['from'] == "north") & (raw_data['to'] == "west")]
+
+        eastToEast = raw_data.loc[(raw_data['from'] == "east") & (raw_data['to'] == "east")]
+        eastToSouth = raw_data.loc[(raw_data['from'] == "east") & (raw_data['to'] == "south")]
+        eastToNorth = raw_data.loc[(raw_data['from'] == "east") & (raw_data['to'] == "north")]
+        eastToWest = raw_data.loc[(raw_data['from'] == "east") & (raw_data['to'] == "west")]
+
+        westToWest = raw_data.loc[(raw_data['from'] == "west") & (raw_data['to'] == "west")]
+        westToSouth = raw_data.loc[(raw_data['from'] == "west") & (raw_data['to'] == "south")]
+        westToNorth = raw_data.loc[(raw_data['from'] == "west") & (raw_data['to'] == "north")]
+        westToEast = raw_data.loc[(raw_data['from'] == "west") & (raw_data['to'] == "east")]
+
+        # Counting cars for each directions.
+        ssCount = southToSouth.shape[0]
+        snCount = southToNorth.shape[0]
+        seCount = southToEast.shape[0]
+        swCount = southToWest.shape[0]
+
+        nnCount = northToNorth.shape[0]
+        nsCount = northToSouth.shape[0]
+        neCount = northToEast.shape[0]
+        nwCount = northToWest.shape[0]
+
+        eeCount = eastToEast.shape[0]
+        esCount = eastToSouth.shape[0]
+        enCount = eastToNorth.shape[0]
+        ewCount = eastToWest.shape[0]
+
+        wwCount = westToWest.shape[0]
+        wsCount = westToSouth.shape[0]
+        wnCount = westToNorth.shape[0]
+        weCount = westToEast.shape[0]
+
+        # Counting cars going straight and turning right.
+        northSouthCount = ssCount + snCount + nnCount + nsCount + seCount + nwCount
+        eastWestCount = eeCount + ewCount + wwCount + weCount + enCount + wsCount
+
+        # Counting cars turning left.
+        northSouthLeftCount = swCount + neCount
+        eastWestLeftCount = wnCount + esCount
+
+        # Calculating ratios for all directions
+        if (northSouthCount == 0):
+            nsLeftPercentage = 0
+        else: 
+            nsLeftPercentage = (northSouthLeftCount/northSouthCount)*100
+        
+        if (eastWestCount == 0):
+            ewLeftPercentage = 0
+            straightRatio = 10
+
+        else: 
+            ewLeftPercentage = (eastWestLeftCount/eastWestCount)*100
+            straightRatio = (northSouthCount/eastWestCount)
+
+        # Setting timers: sum(timers) = 80s
+        timerSum = 80
+
+        # left turn
+        if (nsLeftPercentage > 40 or ewLeftPercentage > 40):
+            leftTurnTimer = 25
+        elif (40 > nsLeftPercentage > 25 or 40 > ewLeftPercentage > 25):
+            leftTurnTimer = 20
+        else: 
+            leftTurnTimer = 15
+        
+        timerSum = timerSum - leftTurnTimer
+
+        # straight and right turn
+        # more cars coming from north and south
+        if (1 < straightRatio < 1.5):
+            northSouthTimer = ((60/100)*timerSum)
+            eastWestTimer = ((40/100)*timerSum)
+        elif (1.5 < straightRatio < 2):
+            northSouthTimer = ((65/100)*timerSum)
+            eastWestTimer = ((35/100)*timerSum)
+        elif (straightRatio > 2): 
+            northSouthTimer = ((70/100)*timerSum)
+            eastWestTimer = ((30/100)*timerSum)
+        # more cars coming from east and west
+        elif (0.5 < straightRatio < 1):
+            northSouthTimer = ((40/100)*timerSum)
+            eastWestTimer = ((60/100)*timerSum)
+        elif (0.25 < straightRatio < 0.5):
+            northSouthTimer = ((35/100)*timerSum)
+            eastWestTimer = ((65/100)*timerSum)
+        elif (straightRatio < 0.25): 
+            northSouthTimer = ((30/100)*timerSum)
+            eastWestTimer = ((70/100)*timerSum)
+        # equal number of cars from both sides
+        else: 
+            northSouthTimer = ((50/100)*timerSum)
+            eastWestTimer = ((50/100)*timerSum)
+
+        self.timers['north-south'] = northSouthTimer
+        self.timers['east-west'] = eastWestTimer
+        self.timers['left'] = leftTurnTimer
+        
--- project/backend/backend_django/camera/real_time/tests.py
+++ project/backend/backend_django/camera/real_time/tests.py
@@ -4,42 +4,52 @@ import os
 import io
 import unittest
 import mongomock
+import datetime
 import pandas as panda
 from freezegun import freeze_time
 from realtime import Realtime
 from unittest.mock import MagicMock, patch
 sys.modules['requests'] = MagicMock()
 sys.modules['Db'] = MagicMock()
-# from ..views import get_timers
+# from views import get_timers
 # from django.http import JsonResponse
 # Create your tests here.
 
-class realtime_test_class(unittest.TestCase):
-    def setUp(self):
-        pass
+class RealTimeTest(unittest.TestCase):
     # def setUp(self):
-    #     df = panda.DataFrame()
-    #     datetime = ['2017-12-31 01:00:10', '2017-12-31 01:00:10', '2017-12-31 01:00:02', '2017-12-31 01:00:20', '2017-12-31 01:00:30', '2017-12-31 01:00:01', '2017-12-31 01:00:40']
-    #     car_id = [1, 2, 3, 4, 5, 6, 7]
-    #     car_from = ['north', 'east', 'east', 'west', 'south', 'north', 'west']
-    #     car_to = ['south', 'north', 'south', 'north', 'east', 'east', 'north']
+    #     pass
+    def setUp(self):
+        df = panda.DataFrame()
+        datetime = ['2017-12-31 01:00:10', '2017-12-31 01:00:10', '2017-12-31 01:00:02', '2017-12-31 01:00:20', '2017-12-31 01:00:30', '2017-12-31 01:00:01', '2017-12-31 01:00:40']
+        car_id = [1, 2, 3, 4, 5, 6, 7]
+        car_from = ['north', 'east', 'east', 'west', 'south', 'north', 'west']
+        car_to = ['south', 'north', 'south', 'north', 'east', 'east', 'north']
 
-    #     df['id'] = car_id
-    #     df['datetime'] = datetime
-    #     df['from'] = car_from
-    #     df['to'] = car_to
+        df['id'] = car_id
+        df['datetime'] = datetime
+        df['from'] = car_from
+        df['to'] = car_to
 
-    #     return df
+        return df
 
-    # @freeze_time("2017-01-14 01:00:20")
-    # def test_realtime(self):
-    #     df = self.setUp()
-    #     real_time_object = Realtime()
-    #     self.assertEquals(real_time_object.get_milli_time(), 12030000)
-    #     real_time_object.det_timers()
-    #     print(real_time_object.timers)
-    
+    @freeze_time("2017-01-14 01:00:20")
+    def test_ms_time(self):
+        real_time_object = Realtime()
+        self.assertEqual(real_time_object.get_milli_time(), 3620000)
+
+    @freeze_time("2017-01-14 01:00:20")
+    def test_timers(self):
+        df = self.setUp()
+        real_time_object = Realtime()
+        real_time_object.det_timers_test(df)
+        self.assertEqual(real_time_object.timers['north-south'], 45.5)
+        self.assertEqual(real_time_object.timers['east-west'], 19.5)
+        self.assertEqual(real_time_object.timers['left'], 15)
+
     # @patch('requests.get')
     # def test_get_timers(self,mock_request):
     #     output = get_timers(mock_request)
     #     self.assertTrue(type(output) is JsonResponse)
+
+if __name__ == '__main__':
+    unittest.main()
