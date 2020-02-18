22c72fcb1b2b721a85176ddd57708e162a47e876
Kevin L <Lin_Kevin_1995@hotmail.com>
2020-02-18 01:32:20 -0500

    #308 #309 Added selector unit tests to check for authentication and staff status.

--- project/frontend/360_dashboard/src/test/contexts/authentication.spec.js
+++ project/frontend/360_dashboard/src/test/contexts/authentication.spec.js
@@ -1,10 +1,66 @@
 import { expect } from 'chai';
 import reducer, {
-  initState, logout, authSuccess, authFail,
+  initState,
+  logout,
+  authSuccess,
+  authFail,
+  authenticated,
+  isStaff,
 } from '../../contexts/authentication';
 
 // AUTHENTICATION TEST
 describe('authentication redux', () => {
+  describe('selector', () => {
+    const loggedOutState = {
+      authentication: {
+        sessionToken: '',
+        username: '',
+        timestamp: '',
+        error: '',
+        user_id: 0,
+        is_staff: false,
+      },
+    };
+
+    const operatorState = {
+      authentication: {
+        sessionToken: 'operator-1',
+        username: 'operator',
+        timestamp: '123',
+        error: '',
+        user_id: 2,
+        is_staff: false,
+      },
+    };
+
+    const adminState = {
+      authentication: {
+        sessionToken: 'admin-1',
+        username: 'admin',
+        timestamp: '123',
+        error: '',
+        user_id: 1,
+        is_staff: true,
+      },
+    };
+    // Authenticated Selector
+    it('check if a user is authenticated', () => {
+      expect(authenticated(operatorState)).to.equal(true);
+    });
+
+    it('check if a user is not authenticated', () => {
+      expect(authenticated(loggedOutState)).to.equal(false);
+    });
+
+    // isStaff selector
+    it('check if a user is a staff', () => {
+      expect(isStaff(adminState)).to.equal(true);
+    });
+
+    it('check if a user is not a staff', () => {
+      expect(isStaff(operatorState)).to.equal(false);
+    });
+  });
   describe('reducer', () => {
     // Authenticate Success
     it('should set username as the username in the input', () => {
@@ -17,6 +73,7 @@ describe('authentication redux', () => {
         authSuccess({
           username: 'TEST',
           user_id: 0,
+          is_staff: false,
         }),
       );
       expect(result).to.include({
@@ -24,6 +81,7 @@ describe('authentication redux', () => {
         username: '',
         user_id: 0,
         error: '',
+        is_staff: false,
       });
     });
     // Authenticate Fail
@@ -44,6 +102,8 @@ describe('authentication redux', () => {
         username: '',
         timestamp: '',
         error: '',
+        user_id: 0,
+        is_staff: false,
       });
     });
   });
