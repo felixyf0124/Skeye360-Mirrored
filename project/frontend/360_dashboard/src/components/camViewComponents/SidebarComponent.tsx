1ea569498de32fb056eca478d2162ec54fc25a2f
lokevin88 <lo_kevin@outlook.com>
2020-02-21 11:22:59 -0500

    #312 Added icons for emergency stop

--- project/frontend/360_dashboard/src/components/camViewComponents/SidebarComponent.tsx
+++ project/frontend/360_dashboard/src/components/camViewComponents/SidebarComponent.tsx
@@ -10,7 +10,7 @@ import DirectionsWalkIcon from '@material-ui/icons/DirectionsWalk';
 import TrackChangesIcon from '@material-ui/icons/TrackChanges';
 import { SKEYE_WHITE, SKEYE_GREY, SKEYE_LIGHT_DARK_GREY, SKEYE_RED, SKEYE_LIGHT_BLACK } from '../../css/custom';
 import Button from '@material-ui/core/Button';
-import styled from 'styled-components';
+import ReportIcon from '@material-ui/icons/Report';
 
 const width = 10;
 
@@ -62,23 +62,23 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 const skeyeStyles = {
-    listItemStyle: {
+    ListItemStyle: {
       color: SKEYE_WHITE,
       fontSize: '0.1em',
     },
-    iconStyle: {
+    IconStyle: {
       color: SKEYE_WHITE,
     },
-    title: {
+    Title: {
       color: SKEYE_WHITE,
       fontSize: 16,
       marginLeft: 4,
     },
-    header: {
+    Header: {
       color: SKEYE_GREY,
       fontSize: 15,
     },
-    offlineButton: {
+    OfflineButton: {
       backgroundColor: SKEYE_LIGHT_DARK_GREY,
       color: SKEYE_WHITE,
       marginTop: 8,
@@ -87,15 +87,17 @@ const skeyeStyles = {
       marginRight: 8,
       width: `${width - 2}vw`,
     },
-    emergencyButton: {
+    EmergencyButton: {
       backgroundColor: SKEYE_LIGHT_DARK_GREY,
       color: SKEYE_RED,
       marginTop: 8,
       marginBottom: 8,
       marginLeft: 8,
-      marginRight: 8,
       width: `${width - 2}vw`,
     },
+    StopIcon: {
+      marginRight: 10,
+    }
 }
 
 export default function SidebarComponent() {
@@ -113,40 +115,40 @@ export default function SidebarComponent() {
     <div className={classes.sidebar}>
       <div className={classes.content}>
         <div className={classes.titleBox}>
-          <text style={ skeyeStyles.title }> Options for Simulator</text>
+          <text style={ skeyeStyles.Title }> Options for Simulator</text>
         </div>
-        <text style={ skeyeStyles.header }> Modes </text>
+        <text style={ skeyeStyles.Header }> Modes </text>
         <Divider classes={{root: classes.dividerGrey}} />
         <List>
           <ListItem button selected={selectedIndex === 0} classes={{ root: classes.root, selected: classes.selected }} onClick={event => onClickListItem(event, 0)}>
-            <ListItemIcon style={ skeyeStyles.iconStyle }>
+            <ListItemIcon style={ skeyeStyles.IconStyle }>
               <TrendingUpIcon />
             </ListItemIcon>
-            <ListItemText style={ skeyeStyles.listItemStyle } >
+            <ListItemText style={ skeyeStyles.ListItemStyle } >
               Arima
             </ListItemText>
           </ListItem>
 
           <ListItem button selected={selectedIndex === 1} classes={{ root: classes.root, selected: classes.selected }} onClick={event => onClickListItem(event, 1)}>
-            <ListItemIcon style={ skeyeStyles.iconStyle }>
+            <ListItemIcon style={ skeyeStyles.IconStyle }>
               <DirectionsWalkIcon />
             </ListItemIcon>
-            <ListItemText style={ skeyeStyles.listItemStyle } >
+            <ListItemText style={ skeyeStyles.ListItemStyle } >
               Pedestrians
             </ListItemText>
           </ListItem>
 
           <ListItem button selected={selectedIndex === 2} classes={{ root: classes.root, selected: classes.selected }} onClick={event => onClickListItem(event, 2)}>
-            <ListItemIcon style={ skeyeStyles.iconStyle }>
+            <ListItemIcon style={ skeyeStyles.IconStyle }>
               <TrackChangesIcon />
             </ListItemIcon>
-            <ListItemText style={ skeyeStyles.listItemStyle } >
+            <ListItemText style={ skeyeStyles.ListItemStyle } >
               Real-Time
             </ListItemText>
           </ListItem>
         </List>
         <Divider classes={{root: classes.dividerWhite}} />
-        <text style={ skeyeStyles.header }> Traffic Light Comparison </text>
+        <text style={ skeyeStyles.Header }> Traffic Light Comparison </text>
         <Divider classes={{root: classes.dividerGrey}} />
 
         <br />
@@ -154,16 +156,16 @@ export default function SidebarComponent() {
         <br />
 
         <Divider classes={{root: classes.dividerWhite}} />
-        <text style={ skeyeStyles.header }> Offline mode </text>
+        <text style={ skeyeStyles.Header }> Simulator </text>
         <Divider classes={{root: classes.dividerGrey}} />
 
-        <Button variant="contained" style={ skeyeStyles.offlineButton }>OFF</Button>
+        <Button variant="contained" style={ skeyeStyles.OfflineButton }>ON</Button>
 
         <Divider classes={{root: classes.dividerWhite}} />
-        <text style={ skeyeStyles.header }> Emergency Stop </text>
+        <text style={ skeyeStyles.Header }> Stop all traffic lights </text>
         <Divider classes={{root: classes.dividerGrey}} />
         
-        <Button variant="contained" style={ skeyeStyles.emergencyButton }>STOP</Button>
+        <Button variant="contained" style={ skeyeStyles.EmergencyButton }> <ReportIcon style={ skeyeStyles.StopIcon }/> STOP</Button>
       </div>
     </div>
   );
