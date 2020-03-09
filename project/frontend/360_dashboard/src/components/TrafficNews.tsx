8b215171ba6cb4fdcef86d92622ac2354d945533
Batoul Yehia <batoul.yehia@gmail.com>
2020-03-08 20:59:23 -0400

    #301 Displays the traffic news based on intersections

--- project/frontend/360_dashboard/src/components/TrafficNews.tsx
+++ project/frontend/360_dashboard/src/components/TrafficNews.tsx
@@ -5,10 +5,8 @@ import styled from 'styled-components';
 
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import AnnouncementIcon from '@material-ui/icons/Announcement';
 import { SKEYE_WHITE, LOW_RES } from '../css/custom';
-import { parse } from '@babel/core';
-
-const { REACT_APP_API_URL } = process.env;
 
 // MapQuest API is used to retrieve traffic news
 // https://developer.mapquest.com/documentation/traffic-api/incidents/get/
@@ -24,20 +22,39 @@ const { REACT_APP_API_URL } = process.env;
   2 = Event
   3 = Congestion/Flow
   4 = Incident/accident
-*/
 
+  Severity Ranges from 0-4, which is an indicator for delay,
+  0 being the lowest, 4 being the highest.
+*/
+const { REACT_APP_API_URL } = process.env;
 const API_KEY = '24jtUJNMCXQg4pLgMchaC7p6Flihs7wO';
 
 const BOUNDING_BOX = '45.7047897,-73.47429525,45.41007553,-73.97290173';
 const API_CALL = `http://www.mapquestapi.com/traffic/v2/incidents?key=${API_KEY}&boundingBox=${BOUNDING_BOX}`;
 
+const API_DOMAIN = REACT_APP_API_URL;
+const DISTRICT_CALL = `//${API_DOMAIN}/api/district/`
+
+var incidentsArray: any[] = [];
+
 interface StateProps {
   error: any;
   isLoaded: boolean;
   incidents: any;
-  intersections: any;
+}
+const createBoundingBox = (latitude: number, longitude: number): string => {
+  const upperBoundLatitude = latitude + 0.1;
+  const upperBoundLongitude = longitude + 0.1; 
+
+  const lowerBoundLatitude = latitude - 0.1;
+  const lowerBoundLongitude = longitude - 0.1; 
+
+  return upperBoundLatitude.toString() + ',' + upperBoundLongitude.toString() + ','  + lowerBoundLatitude.toString() + ',' + lowerBoundLongitude.toString(); 
 }
 
+
+
+//Styled Components 
 const OuterContainer = styled.div`
   overflow: scroll;
   height: 88vh;
@@ -64,26 +81,6 @@ const CardStyle = styled.div`
   }
 `;
 
-const API_DOMAIN = REACT_APP_API_URL;
-const DISTRICT_CALL = `//${API_DOMAIN}/api/district/`
-
-var intersectionResponse: any[] = [];
-//Fetches the intersections of the district
-const createBoundingBox = (latitude: number, longitude: number): string => {
-  const upperBoundLatitude = latitude + 0.001;
-  const upperBoundLongitude = longitude + 0.001; 
-
-  const lowerBoundLatitude = latitude - 0.001;
-  const lowerBoundLongitude = longitude - 0.001; 
-
-
-  console.log(lowerBoundLatitude);
-
-  return upperBoundLatitude.toString() + ',' + upperBoundLongitude.toString() + ','  + lowerBoundLatitude.toString() + ',' + lowerBoundLongitude.toString(); 
-
-
-}
-
 class TrafficNews extends React.Component<{}, StateProps> {
   constructor(props: any) {
     super(props);
@@ -91,16 +88,15 @@ class TrafficNews extends React.Component<{}, StateProps> {
       error: null,
       isLoaded: false,
       incidents: [],
-      intersections: [],
     };
   }
 
-  
   componentDidMount(): void {
+    // eslint-disable-next-line no-shadow
     var latitude: number;
     var longitude: number;
     var boundingBox: string;
-    const districtFetch = (): any => {
+    const districtFetch = () => {
       const url = DISTRICT_CALL;
       const settings = {
         method: 'GET',
@@ -108,37 +104,31 @@ class TrafficNews extends React.Component<{}, StateProps> {
       };
       fetch(url, settings).then(async (response) => {
         const data = await response.json();
-        //console.log(data[0].intersections);
         var CALL = ``;
-  
+
         for(let i = 0; i < data[0].intersections.length; i++){
-          //1. Retrieve the intersection's latitude & longitude
+          //1. Retrieve intersection's latitude and longitude
           latitude = data[0].intersections[i].latitude;
           longitude = data[0].intersections[i].longitude;
 
-          //2. Bounding Box
+          //2. Create a bounding box
           boundingBox = createBoundingBox(latitude, longitude);
-          //console.log(boundingBox);
-          var theResults: any[] = [];
-          //3. Fetch from API & Append to State
 
+          //3. Fetch traffic news from API and append it to state
           CALL = `http://www.mapquestapi.com/traffic/v2/incidents?key=${API_KEY}&boundingBox=${boundingBox}`;
-          //console.log(CALL);
-          var copydata: any[] = [];
+          var tempData: any[] = [];
           fetch(CALL)
             .then((results) => results.json())
             .then(
               (data) => {
-                copydata = data.incidents;
-                if(copydata.length !== 0){
-                  theResults.push(data.incidents);
-                }
+                tempData = data.incidents;
                 
-                console.log(theResults)
-                //console.log('the results' + theResults);
+                if(tempData.length !== 0){
+                  incidentsArray.push(...tempData);
+                }
                 this.setState({
                   isLoaded: true,
-                  incidents: theResults
+                  incidents: incidentsArray
                 });
               },
               (error) => {
@@ -148,21 +138,14 @@ class TrafficNews extends React.Component<{}, StateProps> {
                 })
               }
             )
-      }
-        return data[0].intersections;
+        }
       });
     }
     districtFetch();
-    
-    //console.log(districtFetch);
-    // eslint-disable-next-line no-shadow
   }
 
   render(): JSX.Element {
-    const { error, isLoaded, incidents } = this.state;
-    if (error) {
-      return <div>Error</div>;
-    }
+    const { isLoaded, incidents } = this.state;
     if (!isLoaded) {
       return <CircularProgress />;
     }
@@ -178,29 +161,45 @@ class TrafficNews extends React.Component<{}, StateProps> {
               shortDesc: string;
               fullDesc: string;
               severity: any;
-              startTime: any;
-              endTime: any;
             }) => (
               <CardStyle key={incident.id}>
                 <Card>
                   <CardContent>
-                    <Typography variant="h5">
-                      Traffic Type: &nbsp;
+                    <div style={{ display: 'flex' }}>
+                      <Typography variant="h5">
+                        Traffic Type: &nbsp;
+                        {(() => {
+                          switch (incident.type) {
+                            case 1:
+                              return 'Construction';
+                            case 2:
+                              return 'Event';
+                            case 3:
+                              return 'Congestion/Flow';
+                            case 4:
+                              return 'Incident/Accident';
+                            default:
+                              return 'Traffic';
+                          }
+                        })()}
+                        &nbsp;
+                      </Typography>
+
                       {(() => {
                         switch (incident.type) {
                           case 1:
-                            return 'Construction';
+                            return <AnnouncementIcon style={{ color: 'orange' }} />;
                           case 2:
-                            return 'Event';
+                            return <AnnouncementIcon style={{ color: 'purple' }} />;
                           case 3:
-                            return 'Congestion/Flow';
+                            return <AnnouncementIcon style={{ color: '#d62f2f' }} />;
                           case 4:
-                            return 'Incident/Accident';
+                            return <AnnouncementIcon style={{ color: 'red' }} />;
                           default:
-                            return 'Traffic';
+                            return <AnnouncementIcon />;
                         }
                       })()}
-                    </Typography>
+                    </div>
                     <Typography variant="h6">{incident.shortDesc}</Typography>
                     <Typography variant="subtitle1">
                       {incident.fullDesc}
@@ -208,17 +207,22 @@ class TrafficNews extends React.Component<{}, StateProps> {
                       <br />
                       <b>Severity:</b>
                       {' '}
-                      {incident.severity}
-                      {' '}
-                      <br />
-                      <b>Start Time:</b>
-                      {' '}
-                      {incident.startTime}
-                      {' '}
-                      <br />
-                      <b>End Time:</b>
-                      {' '}
-                      {incident.endTime}
+                      {(() => {
+                        switch (incident.severity) {
+                          case 0:
+                            return 'Very Low Delay';
+                          case 1:
+                            return 'Low Delay';
+                          case 2:
+                            return 'Normal Delay';
+                          case 3:
+                            return 'Medium Delay';
+                          case 4:
+                            return 'High Delay';
+                          default:
+                            return 'Normal Delay';
+                        }
+                      })()}
                     </Typography>
                   </CardContent>
                 </Card>
