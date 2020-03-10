f99b9f3d5878d7c5f20565add378a218fb18d278
Kevin L <Lin_Kevin_1995@hotmail.com>
2020-03-09 22:39:06 -0400

    #354 Intersection list table's title is now aligned on the left side on mobile.

--- project/frontend/360_dashboard/src/components/IntersectionTable.tsx
+++ project/frontend/360_dashboard/src/components/IntersectionTable.tsx
@@ -11,6 +11,7 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import EditIcon from '@material-ui/icons/Edit';
 import LaunchIcon from '@material-ui/icons/Launch';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { STATE as districtState } from '../contexts/districts';
@@ -18,6 +19,7 @@ import DeleteIntersectionButton from './DeleteIntersectionButton';
 import TrafficIntensity from './TrafficIntensity';
 import CameraConnectionStatus from './CameraConnectionStatus';
 import { Response as cameraResponse } from '../api/camera';
+import { MOBILE_DEVICE_MAX_WIDTH } from '../css/custom';
 
 // Generic flexboxes styling
 const VerticalFlexBox = styled.div`
@@ -27,6 +29,16 @@ const VerticalFlexBox = styled.div`
   justify-content: space-between;
   align-items: stretch;
   align-content: stretch;
+`;
+
+// Generic flexboxes styling
+const TableTitle = styled.div`
+  justify-content: center;
+  @media only screen and (max-width: ${MOBILE_DEVICE_MAX_WIDTH}px) {
+    & {
+      justify-content: center;
+    }
+  }
 `;
 
 /* Tables from Material-UI:
@@ -81,15 +93,23 @@ const IntersectionTable = (props: StateProps): JSX.Element => {
   // And displays their information in the table.
   const classes = useStyles();
   const { districts, isStaff, user_id } = props;
+  const isMobile = useMediaQuery(`(max-width:${MOBILE_DEVICE_MAX_WIDTH}px)`);
+
   return (
     <main className={classes.content}>
       <TableContainer component={Paper}>
         <Table className={classes.table} aria-label="intersection table">
           <TableHead>
             <TableRow>
-              <TableCell align="center" colSpan={10} style={{ fontSize: '20px' }}>
-                Intersection List
-              </TableCell>
+              {isMobile ? (
+                <TableCell align="left" colSpan={10} style={{ fontSize: '20px' }}>
+                  <TableTitle>Intersection List</TableTitle>
+                </TableCell>
+              ) : (
+                <TableCell align="center" colSpan={10} style={{ fontSize: '20px' }}>
+                  <TableTitle>Intersection List</TableTitle>
+                </TableCell>
+              )}
             </TableRow>
           </TableHead>
           <TableHead>
