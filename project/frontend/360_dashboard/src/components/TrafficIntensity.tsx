cb5e525072f81c8485a3060af2c32f2bfc4f865a
Kevin L <Lin_Kevin_1995@hotmail.com>
2020-03-18 21:17:01 -0400

    #376 Color coded traffic intensity.

--- project/frontend/360_dashboard/src/components/TrafficIntensity.tsx
+++ project/frontend/360_dashboard/src/components/TrafficIntensity.tsx
@@ -1,8 +1,28 @@
 /* eslint-disable @typescript-eslint/camelcase */
 import React from 'react';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 import { RootState } from '../reducers/rootReducer';
 import { STATE as trafficState, getTraffic, GetTrafficAction } from '../contexts/traffic';
+import {
+  SKEYE_GREY, SKEYE_RED, SKEYE_YELLOW, SKEYE_BRIGHT_GREEN,
+} from '../css/custom';
+
+const OfflineIndicator = styled.h6`
+  color: ${SKEYE_GREY};
+`;
+
+const RedIndicator = styled.h6`
+  color: ${SKEYE_RED};
+`;
+
+const YellowIndicator = styled.h6`
+  color: ${SKEYE_YELLOW};
+`;
+
+const GreenIndicator = styled.h6`
+  color: ${SKEYE_BRIGHT_GREEN};
+`;
 
 interface Props {
   camera_id: number;
@@ -25,33 +45,33 @@ class TrafficIntensity extends React.Component<{} & Props & StateProps & Dispatc
     getTraffic(camera_id, camera_url);
   }
 
-  componentDidUpdate(prevState: StateProps): void {
-    const { traffic: prevTraffic } = prevState;
-    const {
-      traffic, camera_id, camera_url, getTraffic,
-    } = this.props;
-    if (prevTraffic[camera_id] !== traffic[camera_id]) {
-      getTraffic(camera_id, camera_url);
-    }
-  }
+  // componentDidUpdate(prevState: StateProps): void {
+  //   const { traffic: prevTraffic } = prevState;
+  //   const {
+  //     traffic, camera_id, camera_url, getTraffic,
+  //   } = this.props;
+  //   if (prevTraffic[camera_id].los !== traffic[camera_id].los) {
+  //     getTraffic(camera_id, camera_url);
+  //   }
+  // }
 
   // Render Traffic Intensity Metric
   render(): JSX.Element {
     const { camera_id, traffic } = this.props;
     const displayIntensity = (): any => {
       if (traffic[camera_id] === undefined) {
-        return 'N/A';
+        return <OfflineIndicator>N/A</OfflineIndicator>;
       }
       if (traffic[camera_id].los === -1) {
-        return 'N/A';
+        return <OfflineIndicator>N/A</OfflineIndicator>;
       }
       if (traffic[camera_id].los <= 20) {
-        return 'Low';
+        return <GreenIndicator>Low</GreenIndicator>;
       }
       if (traffic[camera_id].los > 55) {
-        return 'High';
+        return <RedIndicator>High</RedIndicator>;
       }
-      return 'Medium';
+      return <YellowIndicator>Medium</YellowIndicator>;
     };
     return <div>{displayIntensity()}</div>;
   }
